456235d469c3579764e407bced2d1c6040473685
kongkdh <kongkdh@kong-tech.com>
2021-08-26 15:11:44 +0900

    feat: test for how to make job instance with job parameters

--- src/main/java/com/example/springbatchtest/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springbatchtest/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,17 +23,20 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob(){
         return jobBuilderFactory.get("simpleJob") //"simpleJob이라는 job을 생성합니다."
-                .start(simpleStep1()) // "simpleJob은 simpleStep1을 step으로 가지고 있습니다"
+                .start(simpleStep1(null)) // "simpleJob은 simpleStep1을 step으로 가지고 있습니다"
                 .build();
     }
 
     @Bean
-    public Step simpleStep1(){
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate){
         return stepBuilderFactory.get("simpleStep1") //"simpleStep1이라는 Step을 생성합니다."
                 .tasklet((contribution, chunkContext) -> { //각각의 Step 안에서 수행될 기능들을 명시합니다.
                     log.info("스텝 1단계 시작");  //tasklet에서 수행될 기능들을 명시하고
+                    log.info(">>>>>>>>>>>>> requestDate = {}" , requestDate);
                     return RepeatStatus.FINISHED; // reader,processor,write에서 실질적인 작업을 수행한다.
                 })
                 .build();
     }
+
 }
